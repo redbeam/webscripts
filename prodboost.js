0b0273a58246a2e1aa2a0ac90e90f0578c36f4b8
redbeam_ <skvarlamatus@gmail.com>
2020-02-05 22:56:04 +0100

    Create prodboost.js

---
+++ prodboost.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         YouTube Productivity Booster
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Hides suggested videos on YouTube
+// @author       Matus
+// @match        *://www.youtube.com/*
+// @grant        unsafeWindow
+// ==/UserScript==
+
+console.log("Starting YouTube Productivity Booster");
+
+function hideOdporucane() {
+    var odporucane = document.getElementsByClassName("section-list")[0] || document.getElementById("watch7-sidebar-modules");
+    var odporucaneOrig = odporucane.innerHTML;
+
+    odporucane.innerHTML = "<a id=\"odporucaneUnhide\">Odporúčané videá skryté; Kliknite pre odkrytie</a>";
+
+    document.getElementById("odporucaneUnhide").addEventListener("click", function() {
+        odporucane.innerHTML = odporucaneOrig;
+    });
+}
+
+setTimeout(function() { // first start
+    if (document.location.pathname == "/" || document.location.pathname == "/watch") {
+        hideOdporucane();
+    }
+}, 1000);
+
+unsafeWindow.addEventListener("spfdone", function() { // after navigation
+    setTimeout(function() {
+        if (document.location.pathname == "/" || document.location.pathname == "/watch") {
+            hideOdporucane();
+        }
+    }, 1000);
+});
+
+// OLD VERSION
+/*var skript = document.createElement('script');
+skript.type = 'text/javascript';
+skript.textContent = '\n\
+var odporucane;\n\
+var odporucaneOrigContent;\n\
+\n\
+function hideOdporucane() {\n\
+    odporucane = document.getElementsByClassName("section-list")[0] || document.getElementById("watch7-sidebar-modules");\n\
+    odporucaneOrigContent = odporucane.innerHTML;\n\
+    \n\
+    odporucane.innerHTML = "<a onclick=\\"odporucane.innerHTML = odporucaneOrigContent;\\">Odporúčané videá skryté; Kliknite pre odkrytie</a>";\n\
+}\n\
+';
+document.getElementsByTagName("head")[0].appendChild(skript);*/
