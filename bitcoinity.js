903d229ee81c1ddfc67219ec43f3b8861c55627a
redbeam_ <skvarlamatus@gmail.com>
2020-04-07 02:07:53 +0200

    Update bitcoinity.js
    this userscript no longer periodically overwrites the title tag, now it overwrites the function (once) that the page uses for changing the title -> works better

--- bitcoinity.js
+++ bitcoinity.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bitcoinity.org Title Unspoiler
-// @version      1
-// @description  Masks the bitcoin price in the webpage title
+// @version      2
+// @description  Masks the Bitcoin price in the webpage title
 // @author       Matus
 // @match        http://bitcoinity.org/markets/*
 // @grant        none
@@ -10,8 +10,9 @@
 (function() {
     'use strict';
 
-    setInterval(function() {
-        document.getElementsByTagName("title")[0].innerHTML = "Bitcoinity.org";
-    }, 1);
+    window.Markets.set_title = function() {
+        return document.title = "Bitcoinity.org";
+    }
+    window.Markets.set_title();
 
 })();
