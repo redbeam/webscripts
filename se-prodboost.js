7f4152d5acd0c09119e06fde704a94eea14d57f5
redbeam_ <skvarlamatus@gmail.com>
2020-05-09 21:57:02 +0200

    removed old code

--- se-prodboost.js
+++ se-prodboost.js
@@ -1,7 +1,6 @@
 // ==UserScript==
 // @name         StackExchange Productivity Booster
-// @namespace    http://tampermonkey.net/
-// @version      1
+// @version      2
 // @description  Hides Top Questions on StackExchange sites.
 // @author       Matus
 // @match        *://*.stackexchange.com/*
@@ -25,13 +24,3 @@ hot.innerHTML = "<a id=\"hotUnhide\">Hot Questions hidden; Click to unhide</a>";
 document.getElementById("hotUnhide").addEventListener("click", function() {
     hot.innerHTML = hotOrig;
 });
-
-/*var skript = document.createElement('script');
-skript.type = 'text/javascript';
-skript.textContent = '\n\
-var hot = document.getElementById("hot-network-questions");\n\
-var hotOrig = hot.innerHTML;\n\
-\n\
-hot.innerHTML = "<a onclick=\\"hot.innerHTML = hotOrig\\">Hot Questions hidden; Click to unhide</a>";\n\
-';
-document.getElementsByTagName("head")[0].appendChild(skript);*/
