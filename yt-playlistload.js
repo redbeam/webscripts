74c57a54d99fb886ffff72c280c932373640f389
redbeam_ <skvarlamatus@gmail.com>
2020-08-02 22:35:29 +0200

    Update yt-playlistload.js

--- yt-playlistload.js
+++ yt-playlistload.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YouTube Playlist Loader
-// @version      3
+// @version      4
 // @description  Automatically loads whole playlists on YouTube
 // @author       Matus
 // @match        *://www.youtube.com/*
@@ -13,12 +13,13 @@
     console.log("Starting YouTube Playlist Loader");
 
     var interval = setInterval(function() {
-        if (document.location.pathname == "/playlist") {
+        if (document.location.pathname == "/playlist" && document.getElementsByClassName("pl-header-title").length > 0) {
             try {
                 document.getElementsByClassName("load-more-text")[0].click();
             } catch (err) {
                 clearInterval(interval);
                 window.scrollTo(0, document.body.scrollHeight);
+                console.log("Playlist loaded");
             }
         }
     }, 500);
