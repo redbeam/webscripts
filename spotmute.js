521770b705c13fe2eb660b2b2745058eb5aee69c
redbeam_ <skvarlamatus@gmail.com>
2021-04-12 04:26:00 +0200

    Update spotmute.js

--- spotmute.js
+++ spotmute.js
@@ -1,59 +1,48 @@
 // ==UserScript==
 // @name         Spotify Ad Muter
-// @version      1
+// @version      2
 // @description  Mutes ads on Spotify
 // @author       Matus
 // @match        https://open.spotify.com/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+'use strict';
 
-    // mute button is not present when the script finishes loading,
-    // so we have to get it like this
-    var mutebut = undefined;
-    function getMutebut() {
-        if (mutebut === undefined) {
-            mutebut = document.getElementsByClassName("control-button volume-bar__icon")[0];
-            setTimeout(function() {
-                getMutebut();
-            }, 500);
+var adtexts = [
+    "Spotify · Spotify",
+    "Advertisement · Spotify",
+    "Advertisement ·"
+];
+
+var mutebutXpath = "/html/body/div[4]/div/div[2]/div[2]/footer/div/div[3]/div/div[3]/button";
+var pageTitle = document.getElementsByTagName("title")[0];
+var mutebut = undefined;
+var isMuted = false; // assuming we're not initially muted
+
+function muteAudio(doMute) {
+    if (doMute) {
+        if (!isMuted) {
+            mutebut.click();
+            isMuted = true;
+        }
+    } else {
+        if (isMuted) {
+            mutebut.click();
+            isMuted = false;
         }
     }
+}
 
-    var adtexts = ["Spotify · Spotify", "Advertisement · Spotify", "Advertisement ·"];
+var pageTitleObserver = new MutationObserver(function() {
+    mutebut = document.evaluate(mutebutXpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
-    var pagetitle = document.getElementsByTagName("title")[0];
-    getMutebut();
-
-    var isMuted = false; // assuming we're not initially muted
-    function audioMute(action) {
-        if (action) { // mute
-            if (!isMuted) {
-                console.log("Muting ad");
-                mutebut.click();
-                isMuted = true;
-            }
-        } else {      // unmute
-            if (isMuted) {
-                console.log("Ad ended, unmuting");
-                mutebut.click();
-                isMuted = false;
-            }
-        }
+    if (adtexts.some(adtext => pageTitle.innerHTML.includes(adtext))) {
+        muteAudio(true);
+    } else {
+        muteAudio(false);
     }
+});
 
-    var pagetitleObserver = new MutationObserver(function() {
-        let titleText = pagetitle.innerHTML;
-        if (titleText.includes(adtexts[0]) || titleText.includes(adtexts[1]) || titleText.includes(adtexts[2])) {
-            audioMute(true);
-        } else {
-            audioMute(false);
-        }
-    });
-
-    pagetitleObserver.observe(pagetitle, {childList: true});
-    console.log("Started Spotify Ad Muter");
-
-})();
+pageTitleObserver.observe(pageTitle, {childList: true});
+console.log("Started Spotify Ad Muter");
