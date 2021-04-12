9526d8c32fd3b8e4c93f75585301a7c3af7e9522
redbeam_ <skvarlamatus@gmail.com>
2021-04-12 03:34:27 +0200

    Update se-prodboost.js

--- se-prodboost.js
+++ se-prodboost.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         StackExchange Productivity Booster
-// @version      2
+// @version      3
 // @description  Hides Top Questions on StackExchange sites.
 // @author       Matus
 // @match        *://*.stackexchange.com/*
@@ -14,13 +14,13 @@
 // @grant        none
 // ==/UserScript==
 
-console.log("Starting StackExchange Productivity Booster");
-
 var hot = document.getElementById("hot-network-questions");
 var hotOrig = hot.innerHTML;
 
-hot.innerHTML = "<a id=\"hotUnhide\">Hot Questions hidden; Click to unhide</a>";
+hot.innerHTML = '<a id="hotUnhide">Hot Questions hidden; Click to unhide</a>';
 
 document.getElementById("hotUnhide").addEventListener("click", function() {
     hot.innerHTML = hotOrig;
 });
+
+console.log("Started StackExchange Productivity Booster");
